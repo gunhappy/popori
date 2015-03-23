12bb58c3310d0a26624497a2098f1e34a7da4810
gunhappy <gunhappy123@gmail.com>
2015-03-23 21:58:06 +0700

    add Background picture

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -2,7 +2,9 @@ var GameLayer = cc.LayerColor.extend({
     init: function() {
         this._super( new cc.Color( 127, 127, 127, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
- 
+        this.Background = new Background();
+        this.Background.setPosition( new cc.Point(screenWidth/2,screenHeight/2));
+        this.addChild(this.Background);
         return true;
     }
 });
